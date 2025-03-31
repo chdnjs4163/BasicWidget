4fe4c333f2ea013c94c86baea0c53fb102715833
chdnjs4163 <gufvnd45@naver.com>
2025-03-31 16:24:57 +0900

    MainActivity 및 이벤트 리스너 및 텍스트 속성 추가 및 오버라이딩 바깥쪽에다가 실행하는 방법 (참조변수로 작성)

--- app/src/main/java/kr/ac/kopo/basicwidget/MainActivity.java
+++ app/src/main/java/kr/ac/kopo/basicwidget/MainActivity.java
@@ -2,6 +2,9 @@ package kr.ac.kopo.basicwidget;
 
 import android.graphics.Color;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
@@ -11,6 +14,9 @@ import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
+    EditText edit1;
+    TextView textResult;
+    Button btnResult;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,7 +28,7 @@ public class MainActivity extends AppCompatActivity {
         TextView text3 = findViewById(R.id.text3);
 
         text1.setTextSize(20);
-        text1.setTextColor(Color.RED);
+        text1.setTextColor(Color.MAGENTA);
         text1.setText("한국폴리텍대학");
 
         text2.setTextSize(30);
@@ -32,5 +38,23 @@ public class MainActivity extends AppCompatActivity {
         text3.setText("Singleline속성Singleline속성Singleline속성Singleline속성Singleline속성Singleline속성Singleline속성Singleline속성Singleline속성Singleline속성");
         text3.setSingleLine(true);
 
+        edit1 = findViewById(R.id.edit1);
+        textResult = findViewById(R.id.textResult);
+        Button btnResult = findViewById(R.id.btnResult);
+
+        btnResult.setOnClickListener(btnListener);
+
+
     }
+
+    View.OnClickListener btnListener = (new View.OnClickListener() {
+        @Override
+        public void onClick(View v) {
+            String univName = edit1.getText().toString();
+            String result = univName + "에 합격하신 것을 진심으로 축하드립니다.";
+            textResult.setText(result);
+            textResult.setTextColor(Color.rgb(143,138,255));
+        }
+
+    });
 }
